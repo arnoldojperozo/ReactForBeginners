f2c583129f00c5d36f32117ff9c0c9b3ba068b7e
Arnoldo J. Perozo Mena <arnoldo.perozo@hotmail.com>
2021-09-11 17:34:11 -0600

    Class & Functional Components

---
+++ src/components/Demo.js
@@ -0,0 +1,14 @@
+//const { Component } = require("react");
+import React, {Component} from "react";
+
+class Demo extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        );
+    }
+}
+
+export default Demo;
---
+++ src/components/Demo1.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+function Demo1(){
+    return (
+        <div>
+            <h1>Hello from Function Component</h1>
+        </div>
+    );
+}
+
+export default Demo1;
