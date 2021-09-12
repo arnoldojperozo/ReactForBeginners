20c4bc6dd3576160d93c64c16f7d3d9bace0bc5e
Arnoldo J. Perozo Mena <arnoldo.perozo@hotmail.com>
2021-09-11 18:18:08 -0600

    Functional Components

--- src/components/Demo1.js
+++ src/components/Demo1.js
@@ -1,5 +1,7 @@
-import React, {Component} from "react";
+//import React, {Component} from "react";
 
+//Option 1
+/*
 function Demo1(){
     return (
         <div>
@@ -7,5 +9,24 @@ function Demo1(){
         </div>
     );
 }
+*/
+
+//Option 2
+/*
+const Demo1 = () =>{
+    return (
+        <div>
+            <h1>Hello from Function Component</h1>
+        </div>
+    )
+}
+*/
+
+//Option 3
+const Demo1 = () => (
+    <div>
+        <h1>Hello From function Component</h1>
+    </div>
+)
 
 export default Demo1;
