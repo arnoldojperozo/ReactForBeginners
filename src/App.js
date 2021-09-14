73cc14f13c92e131991f8a5ef5044eda846aae63
Arnoldo J. Perozo Mena <arnoldo.perozo@hotmail.com>
2021-09-13 18:27:41 -0600

    Todo Apps CRUD

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import Sample from './components/Sample';
 import './App.css';
+import TodoList from './components/ToDoList';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash, faPlus, faEdit } from '@fortawesome/free-solid-svg-icons';
+library.add(faTrash, faEdit, faPlus);
 
 function App() {
   return (
     <div className="App">
       <h1>App component</h1>
-      <Sample></Sample>
+      <TodoList></TodoList>
     </div>
   );
 }
